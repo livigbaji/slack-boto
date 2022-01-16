b0897a651eec7c89f630422abf8171980ae95c57
Livinus Igbaji <bestbrain10@gmail.com>
2022-01-16 23:25:46 +0100

    fix failing tests

--- modules/bots/controllers/bots.controller.test.js
+++ modules/bots/controllers/bots.controller.test.js
@@ -15,6 +15,50 @@ describe('Bots Controller', () => {
 
 		await BotsController.bot(req, res, mockNext);
 
-		expect(res.json).toBeCalledWith('yo');
+		expect(res.json).toBeCalledWith({
+			text: 'Would you like to play a game?',
+			response_type: 'in_channel',
+			attachments: [{
+				text: 'Choose a game to play',
+				fallback: 'If you could read this message, you\'d be choosing something fun to do right now.',
+				color: '#3AA3E3',
+				attachment_type: 'default',
+				callback_id: 'game_selection',
+				actions: [{
+					name: 'games_list',
+					text: 'Pick a game...',
+					type: 'select',
+					options: [{
+						text: 'Hearts',
+						value: 'hearts'
+					},
+					{
+						text: 'Bridge',
+						value: 'bridge'
+					},
+					{
+						text: 'Checkers',
+						value: 'checkers'
+					},
+					{
+						text: 'Chess',
+						value: 'chess'
+					},
+					{
+						text: 'Poker',
+						value: 'poker'
+					},
+					{
+						text: 'Falken\'s Maze',
+						value: 'maze'
+					},
+					{
+						text: 'Global Thermonuclear War',
+						value: 'war'
+					}
+					]
+				}]
+			}]
+		});
 	});
 });
