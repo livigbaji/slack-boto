9958716e34050e6317e4c2b0f3778ccb989e20e3
Livinus Igbaji <bestbrain10@gmail.com>
2022-01-16 23:24:34 +0100

    test options response

--- modules/bots/controllers/bots.controller.js
+++ modules/bots/controllers/bots.controller.js
@@ -5,6 +5,51 @@ module.exports = class {
 	static async bot(req, res, next) {
 		// eslint-disable-next-line no-console
 		console.log(req.body);
-		res.json('yo');
+		// const command = req.body.text || '';
+		res.json({
+			text: 'Would you like to play a game?',
+			response_type: 'in_channel',
+			attachments: [{
+				text: 'Choose a game to play',
+				fallback: 'If you could read this message, you\'d be choosing something fun to do right now.',
+				color: '#3AA3E3',
+				attachment_type: 'default',
+				callback_id: 'game_selection',
+				actions: [{
+					name: 'games_list',
+					text: 'Pick a game...',
+					type: 'select',
+					options: [{
+						text: 'Hearts',
+						value: 'hearts'
+					},
+					{
+						text: 'Bridge',
+						value: 'bridge'
+					},
+					{
+						text: 'Checkers',
+						value: 'checkers'
+					},
+					{
+						text: 'Chess',
+						value: 'chess'
+					},
+					{
+						text: 'Poker',
+						value: 'poker'
+					},
+					{
+						text: 'Falken\'s Maze',
+						value: 'maze'
+					},
+					{
+						text: 'Global Thermonuclear War',
+						value: 'war'
+					}
+					]
+				}]
+			}]
+		});
 	}
 };
